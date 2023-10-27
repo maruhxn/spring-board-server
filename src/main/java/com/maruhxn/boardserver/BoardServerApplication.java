9db028b2fc140e1e4616840f2440847463832f13
maruhxn_ <maruhan1016@gmail.com>
2023-10-28 00:28:08 +0900

    Initial commit :)

---
+++ src/main/java/com/maruhxn/boardserver/BoardServerApplication.java
@@ -0,0 +1,13 @@
+package com.maruhxn.boardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maruhxn/boardserver/BoardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maruhxn.boardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
