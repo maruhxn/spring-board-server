7c30834c4fe68a9120c28ce40afc7ed1b2ac77c9
maruhxn_ <maruhan1016@gmail.com>
2023-11-11 20:43:52 +0900

    Add #7 - AuthController 추가

--- src/main/java/com/maruhxn/boardserver/controller/AuthController.java
+++ src/main/java/com/maruhxn/boardserver/controller/AuthController.java
@@ -1,9 +1,15 @@
 package com.maruhxn.boardserver.controller;
 
+import com.maruhxn.boardserver.domain.Member;
 import com.maruhxn.boardserver.dto.request.auth.ConfirmPasswordRequest;
 import com.maruhxn.boardserver.dto.request.auth.LoginRequest;
 import com.maruhxn.boardserver.dto.request.auth.RegisterRequest;
 import com.maruhxn.boardserver.dto.response.ResponseDto;
+import com.maruhxn.boardserver.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,29 +17,43 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseDto<Object> register(@RequestBody RegisterRequest registerRequest) {
+    public ResponseDto<Object> register(@RequestBody @Valid RegisterRequest registerRequest) {
+        authService.register(registerRequest);
+        log.info("회원가입 = {}", registerRequest);
         return ResponseDto.empty("회원가입 성공");
     }
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseDto<Object> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseDto<Object> login(@RequestBody @Valid LoginRequest loginRequest, HttpSession session) {
+        Member loginMember = authService.login(loginRequest);
+        session.setAttribute("member", loginMember);
         return ResponseDto.empty("로그인 성공");
     }
 
     @PostMapping("/{memberId}/password")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseDto<Object> confirmPassword(@RequestBody ConfirmPasswordRequest confirmPasswordRequest) {
+    public ResponseDto<Object> confirmPassword(
+            @RequestBody @Valid ConfirmPasswordRequest confirmPasswordRequest,
+            @PathVariable Long memberId
+    ) {
+        authService.confirmPassword(memberId, confirmPasswordRequest);
         return ResponseDto.empty("비밀번호 인증 성공");
     }
 
     @DeleteMapping("/logout")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void logout() {
-        return;
+    public void logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
     }
 }
