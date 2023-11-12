eec2c8d78e4b1d4a432d55c3d6a77b12c62f3776
maruhxn_ <maruhan1016@gmail.com>
2023-11-12 15:38:28 +0900

    Add #9 - Member RequestDTO에 Bean Validation 추가

--- src/main/java/com/maruhxn/boardserver/dto/request/members/UpdatePasswordRequest.java
+++ src/main/java/com/maruhxn/boardserver/dto/request/members/UpdatePasswordRequest.java
@@ -1,5 +1,6 @@
 package com.maruhxn.boardserver.dto.request.members;
 
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,10 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdatePasswordRequest {
+    @Size(min = 2, max = 20)
     private String currPassword;
+    @Size(min = 2, max = 20)
     private String newPassword;
+    @Size(min = 2, max = 20)
     private String confirmNewPassword;
 }
