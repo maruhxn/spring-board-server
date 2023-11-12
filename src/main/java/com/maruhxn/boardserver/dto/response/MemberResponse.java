cad24499a4bdeccabb06a5fb775a69ce8e9e0020
maruhxn_ <maruhan1016@gmail.com>
2023-11-12 15:39:05 +0900

    Refactor #9 - MemberResponse에 Builder 패턴 추가 컨트롤러 계층에 Entity를 노출하지 않기 위해 Service에서 Entity를 DTO로 변환 후 반환

--- src/main/java/com/maruhxn/boardserver/dto/response/MemberResponse.java
+++ src/main/java/com/maruhxn/boardserver/dto/response/MemberResponse.java
@@ -1,8 +1,10 @@
 package com.maruhxn.boardserver.dto.response;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class MemberResponse {
     private final Long memberId;
     private final String email;
