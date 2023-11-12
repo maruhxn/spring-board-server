0bd8b656af7a54c55a63fee4672c89226d8eb0c9
maruhxn_ <maruhan1016@gmail.com>
2023-11-12 19:33:02 +0900

    Add #11 - FileService 추가

---
+++ src/main/java/com/maruhxn/boardserver/service/FileService.java
@@ -0,0 +1,130 @@
+package com.maruhxn.boardserver.service;
+
+import com.maruhxn.boardserver.common.ErrorCode;
+import com.maruhxn.boardserver.domain.PostImage;
+import com.maruhxn.boardserver.exception.GlobalException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class FileService {
+    @Value("${file.upload_dir}")
+    private String fileDir;
+
+    /**
+     * 파일 단건 저장
+     *
+     * @param file
+     * @return
+     */
+    public PostImage storeOneFile(MultipartFile file) {
+        if (file.isEmpty()) throw new GlobalException(ErrorCode.BAD_REQUEST, "파일이 존재하지 않습니다.");
+
+        String originalFilename = file.getOriginalFilename(); // 파일 원본 이름
+        String storeFileName = createStoreFileName(originalFilename); // 서버에 저장된 파일 이름 (랜덤)
+        String savePath = getFullPath(storeFileName); // 서버에 저장된 경로
+        try {
+            file.transferTo(new File(savePath)); // 파일 저장
+        } catch (IOException e) {
+            log.warn(e.getMessage());
+            throw new GlobalException(ErrorCode.INTERNAL_ERROR, e);
+        }
+
+        return PostImage.builder()
+                .originalName(originalFilename)
+                .storedName(storeFileName)
+                .build();
+    }
+
+    /**
+     * 파일 전체 저장
+     *
+     * @param multipartFiles
+     * @return
+     */
+    public List<PostImage> storeFiles(List<MultipartFile> multipartFiles) {
+        List<PostImage> storedFileURLResult = new ArrayList<>();
+        multipartFiles.forEach(multipartFile ->
+                storedFileURLResult.add(storeOneFile(multipartFile)));
+        return storedFileURLResult;
+    }
+
+    /**
+     * 이미지 조회
+     *
+     * @param fileName
+     * @return
+     */
+    public Resource getImage(String fileName) {
+        Resource resource = null;
+
+        try {
+            resource = new UrlResource("file:" + getFullPath(fileName));
+        } catch (Exception e) {
+            log.warn(e.getMessage());
+            throw new GlobalException(ErrorCode.INTERNAL_ERROR, e);
+        }
+        return resource;
+    }
+
+    /**
+     * 서버에 저장될 실제 경로 생성
+     *
+     * @param fileName
+     * @return
+     */
+    private String getFullPath(String fileName) {
+        return fileDir + fileName;
+    }
+
+    /**
+     * 서버에 저장될 파일명 생성
+     *
+     * @param originalFilename
+     * @return
+     */
+    private String createStoreFileName(String originalFilename) {
+        String ext = extractExt(originalFilename);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + ext;
+    }
+
+    /**
+     * 파일 확장자 추출
+     *
+     * @param originalFilename
+     * @return
+     */
+    private String extractExt(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+
+    /**
+     * 실제 물리 이미지 삭제
+     *
+     * @param storeFileName
+     */
+    public void deleteFile(String storeFileName) {
+        String savePath = getFullPath(storeFileName); // 서버에 저장된 경로
+        try {
+            File file = new File(savePath);
+            System.out.println("file = " + file);
+            file.delete();
+        } catch (Exception e) {
+            log.warn(e.getMessage());
+            throw new GlobalException(ErrorCode.INTERNAL_ERROR, e);
+        }
+    }
+}
