14c57245f5e7b991a6bcd3ec692b39bb4753f7e2
maruhxn_ <maruhan1016@gmail.com>
2023-11-13 10:48:10 +0900

    Add #15 - Interceptor, ArgumentResolver 등록

--- src/main/java/com/maruhxn/boardserver/config/WebConfig.java
+++ src/main/java/com/maruhxn/boardserver/config/WebConfig.java
@@ -1,8 +1,17 @@
 package com.maruhxn.boardserver.config;
 
+import com.maruhxn.boardserver.interceptor.LogInCheckInterceptor;
+import com.maruhxn.boardserver.interceptor.LogOutCheckInterceptor;
+import com.maruhxn.boardserver.interceptor.PostAuthorCheckInterceptor;
+import com.maruhxn.boardserver.resolver.LoginMemberArgumentResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -12,5 +21,28 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/members/**");
+        registry.addInterceptor(new LogOutCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/auth/register", "/auth/login");
+        registry.addInterceptor(postAuthorCheckInterceptor())
+                .order(3)
+                .addPathPatterns("/posts/{postId}", "/posts/{postId}/images/{imageId}");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+
+    @Bean
+    public PostAuthorCheckInterceptor postAuthorCheckInterceptor() {
+        return new PostAuthorCheckInterceptor();
     }
 }
