72309c45d5c082ddbe86fce1435e6a421e6dd5c0
maruhxn_ <maruhan1016@gmail.com>
2023-11-17 16:27:38 +0900

    Refactor #29 - CommentJpaRepository Bean 제거

--- src/main/java/com/maruhxn/boardserver/repository/CommentJpaRepository.java
+++ src/main/java/com/maruhxn/boardserver/repository/CommentJpaRepository.java
@@ -4,12 +4,11 @@ import com.maruhxn.boardserver.common.Constants;
 import com.maruhxn.boardserver.domain.Comment;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+//@Repository
 @RequiredArgsConstructor
 public class CommentJpaRepository {
     private final EntityManager em;
