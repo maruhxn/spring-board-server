977e392a6abdc3c22da9e8d3797d2cc08a2cd210
maruhxn_ <maruhan1016@gmail.com>
2023-12-04 23:21:05 +0900

    Chore #37 - Prod, Dev, Test 환경 분리 by Profile

--- src/main/java/com/maruhxn/boardserver/config/SessionConfig.java
+++ src/main/java/com/maruhxn/boardserver/config/SessionConfig.java
@@ -9,15 +9,18 @@ import com.maruhxn.boardserver.auth.common.AjaxAuthenticationTokenMixin;
 import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.security.jackson2.SecurityJackson2Modules;
 
 @Configuration
+@Profile("prod")
 public class SessionConfig implements BeanClassLoaderAware {
     private ClassLoader loader;
 
     @Bean
+    @Profile("prod")
     public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
         return new GenericJackson2JsonRedisSerializer(objectMapper());
     }
