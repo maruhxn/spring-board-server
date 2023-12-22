f5a8626b5e09e7c09432b9286b5c9aab39284251
maruhxn_ <maruhan1016@gmail.com>
2023-12-22 19:01:49 +0900

    Fix - schema.sql 오류 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,44 +2,38 @@ create table if not exists comment
 (
     comment_id
     bigint
-    generated
-    by
-    default as
-    identity,
-    content
-    TEXT
     not
-    null,
+    null
+    auto_increment,
     created_at
-    timestamp
+    datetime
 (
     6
 ),
     member_id bigint,
     post_id bigint,
+    content TEXT not null,
     primary key
 (
     comment_id
 )
-    );
+    ) engine=InnoDB;
 
 create table if not exists member
 (
-    member_id
-    bigint
-    generated
-    by
-    default as
-    identity,
-    created_at
-    timestamp
+    created_at datetime
 (
     6
 ),
-    updated_at timestamp
+    member_id bigint not null auto_increment,
+    updated_at datetime
 (
     6
 ),
+    username varchar
+(
+    10
+) not null,
     email varchar
 (
     30
@@ -49,115 +43,82 @@ create table if not exists member
     255
 ) not null,
     profile_image TEXT not null,
-    role varchar
+    role enum
 (
-    255
-) default 'ROLE_USER' check
-(
-    role
-    in
-(
-    'ROLE_USER',
-    'ROLE_ADMIN'
-)),
-    username varchar
-(
-    10
-) not null,
+    'ROLE_ADMIN',
+    'ROLE_USER'
+) default 'ROLE_USER',
     primary key
 (
     member_id
 )
-    );
+    ) engine=InnoDB;
 
 create table if not exists post
 (
-    post_id
-    bigint
-    generated
-    by
-    default as
-    identity,
-    created_at
-    timestamp
+    created_at datetime
 (
     6
 ),
-    updated_at timestamp
+    member_id bigint,
+    post_id bigint not null auto_increment,
+    updated_at datetime
 (
     6
 ),
+    view_count bigint default 0 not null,
     content TEXT not null,
     title varchar
 (
     255
 ) not null,
-    view_count bigint default 0 not null,
-    member_id bigint,
     primary key
 (
     post_id
 )
-    );
+    ) engine=InnoDB;
 
 create table if not exists post_image
 (
-    post_image_id
-    bigint
-    generated
-    by
-    default as
-    identity,
-    created_at
-    timestamp
+    created_at datetime
 (
     6
 ),
+    post_id bigint,
+    post_image_id bigint not null auto_increment,
     original_name varchar
 (
     255
 ) not null,
     stored_name TEXT not null,
-    post_id bigint,
     primary key
 (
     post_image_id
 )
-    );
+    ) engine=InnoDB;
 
-create index idx__email__username
-    on member (email, username);
-
-alter table if exists member
-drop
-constraint if exists UK_mbmcqelty0fbrvxp1q58dn57t;
-
-alter table if exists member
-    add constraint UK_mbmcqelty0fbrvxp1q58dn57t unique (email);
-
-alter table if exists member
-drop
-constraint if exists UK_gc3jmn7c2abyo3wf6syln5t2i;
-
-alter table if exists member
+alter table member
     add constraint UK_gc3jmn7c2abyo3wf6syln5t2i unique (username);
 
-alter table if exists comment
+alter table member
+    add constraint UK_mbmcqelty0fbrvxp1q58dn57t unique (email);
+
+alter table comment
     add constraint FKmrrrpi513ssu63i2783jyiv9m
-    foreign key (member_id)
-    references member;
+        foreign key (member_id)
+            references member (member_id);
 
-alter table if exists comment
+alter table comment
     add constraint FKs1slvnkuemjsq2kj4h3vhx7i1
-    foreign key (post_id)
-    references post;
+        foreign key (post_id)
+            references post (post_id);
 
-alter table if exists post
+alter table post
     add constraint FK83s99f4kx8oiqm3ro0sasmpww
-    foreign key (member_id)
-    references member;
+        foreign key (member_id)
+            references member (member_id);
 
-alter table if exists post_image
+alter table post_image
     add constraint FKsip7qv57jw2fw50g97t16nrjr
-    foreign key (post_id)
-    references post;
+        foreign key (post_id)
+            references post (post_id);
