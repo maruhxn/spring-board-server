cdec3757fac1e19e8ba10e7f10ec230bcbc2f460
maruhxn_ <maruhan1016@gmail.com>
2023-12-22 16:48:40 +0900

    Refactor - Member Table 불필요 index 제거

--- src/main/java/com/maruhxn/boardserver/domain/Member.java
+++ src/main/java/com/maruhxn/boardserver/domain/Member.java
@@ -12,10 +12,6 @@ import org.springframework.util.StringUtils;
 import java.io.Serializable;
 
 @Entity
-@Table(
-        indexes = {
-                @Index(name = "idx__email__username", columnList = "email, username")
-        }) // (email), (email, username) cover
 @Getter
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
